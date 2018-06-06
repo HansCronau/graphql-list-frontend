420f34b6606b4c5f2ed6a22b2765da6fb2d4bdac
Hans Cronau <hcronau@ecochain.com>
2018-06-06 14:42:32 +0200

    Makes FE use Peter's BE.

--- graphql-experiment/src/index.js
+++ graphql-experiment/src/index.js
@@ -7,7 +7,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql'
+    // uri: 'http://localhost:4000/graphql'
+    uri: 'http://192.168.1.15:4000/graphql'
 });
 
 const ApolloApp = AppComponent => (
