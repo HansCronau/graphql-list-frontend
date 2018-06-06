f4cd321460830cefddbf305045e572761212be1c
Hans Cronau <hcronau@ecochain.com>
2018-06-06 13:42:52 +0200

    Setup component `App` to provide `ApolloProvider` configured to connect to localhost:9002.

--- graphql-experiment/src/index.js
+++ graphql-experiment/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: 'localhost:9002'
+});
+
+const ApolloApp = AppComponent => (
+<ApolloProvider client={client}>
+    <AppComponent />
+</ApolloProvider>
+);
+
+ReactDOM.render(ApolloApp(App), document.getElementById('root'));
 registerServiceWorker();
